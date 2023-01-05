57beab523bf867c461f62eecf3cb6ce6be13530f
Viktor Szépe <viktor@szepe.net>
2023-01-05 15:57:22 +0000

    Remove WPCF7 Mail (2) domain error too

--- mu-plugins/contact-form-7-sender-error.php
+++ mu-plugins/contact-form-7-sender-error.php
@@ -5,6 +5,7 @@ add_action(
     'wpcf7_config_validator_validate',
     static function ($configValidator) {
         $configValidator->remove_error('mail.sender', WPCF7_ConfigValidator::error_email_not_in_site_domain);
+        $configValidator->remove_error('mail_2.sender', WPCF7_ConfigValidator::error_email_not_in_site_domain);
     },
     10,
     1
