e6cc2256c53f958f655123f4b825d272d1a1e864
Viktor Szépe <viktor@szepe.net>
2023-02-10 00:53:38 +0000

    Add Deprecation logger MU plugin

---
+++ mu-plugins/_core-log-deprecated.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Plugin Name: Deprecation logger
+ */
+
+// Log usage of deprecated WordPress features.
+array_map(
+    static function ($hook) {
+        add_filter(
+            $hook,
+            static function ($deprecated) use ($hook) {
+                error_log(sprintf('WordPress %s: %s', $hook, $deprecated));
+            },
+            0,
+            1
+        );
+    },
+    [
+        'deprecated_function_run',
+        'deprecated_constructor_run',
+        'deprecated_file_included',
+        'deprecated_argument_run',
+        'deprecated_hook_run',
+    ]
+);
