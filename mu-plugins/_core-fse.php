a5383be9e770544937b36341a2d085e33fc41645
Viktor Szépe <viktor@szepe.net>
2023-08-04 11:09:47 +0000

    Move TODO to GitHub Issues https://github.com/szepeviktor/wordpress-website-lifecycle/issues/10

--- mu-plugins/_core-fse.php
+++ mu-plugins/_core-fse.php
@@ -12,8 +12,6 @@ add_action(
         // remove_action('wp_enqueue_scripts', 'wp_common_block_scripts_and_styles');
         remove_action('wp_footer', 'wp_enqueue_global_styles', 1);
         remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
-        // TODO Is this part of one of the hooks above?
-        // remove_filter('render_block', 'wp_render_duotone_support', 10, 2);
     },
     100,
     0
