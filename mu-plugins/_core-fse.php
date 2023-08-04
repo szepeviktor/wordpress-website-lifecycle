27650b66f577d15a262774715caf684a57f687c9
Viktor Szépe <viktor@szepe.net>
2023-08-04 16:36:52 +0000

    Remove block-library CSS

--- mu-plugins/_core-fse.php
+++ mu-plugins/_core-fse.php
@@ -9,7 +9,6 @@ add_action(
     'wp',
     static function () {
         remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
-        // remove_action('wp_enqueue_scripts', 'wp_common_block_scripts_and_styles');
         remove_action('wp_footer', 'wp_enqueue_global_styles', 1);
         remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
     },
