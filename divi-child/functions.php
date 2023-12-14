035c9bbbaae2b9762c81964ae91ee5c5a46e1b03
Viktor Szépe <viktor@szepe.net>
2023-12-14 12:53:11 +0000

    Add page_for_posts cannot be a Divi page

--- divi-child/functions.php
+++ divi-child/functions.php
@@ -10,6 +10,17 @@
 
 // Elegant Themes API call: core/components/Updates.php:573
 
+/*
+// The Blog Posts Index page / home.php / `page_for_posts` cannot be a Divi page
+// https://help.elegantthemes.com/en/articles/2188833-blog-page-not-changing-when-built-with-divi-builder
+add_filter(
+    'pre_option_page_for_posts',
+    '__return_zero',
+    PHP_INT_MAX,
+    0
+);
+*/
+
 function prefix_theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
