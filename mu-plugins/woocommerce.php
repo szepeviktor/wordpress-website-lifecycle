acc24a125b153a604cb6e349ddf5b48d18204fb8
Viktor Szépe <viktor@szepe.net>
2024-07-12 01:03:17 +0000

    Disable WC features

--- mu-plugins/woocommerce.php
+++ mu-plugins/woocommerce.php
@@ -1,15 +1,47 @@
 <?php
 
 /*
- * Plugin Name: Remove WooCommerce guided tour videos
+ * Plugin Name: Remove WooCommerce features
  * Plugin URI: https://github.com/szepeviktor/wordpress-website-lifecycle
  */
 
-add_action(
-    'current_screen',
-    static function () {
-        get_current_screen()->remove_help_tab('woocommerce_guided_tour_tab');
+add_filter(
+    'woocommerce_admin_features',
+    static function ($features) {
+        // From includes/react-admin/feature-config.php
+        $disabled_features = [
+            'activity-panels',
+            'analytics',
+            'product-block-editor',
+            'coupons',
+            'core-profiler',
+            'customize-store',
+            'customer-effort-score-tracks',
+            'import-products-task',
+            'experimental-fashion-sample-products',
+            'shipping-smart-defaults',
+            'shipping-setting-tour',
+            'homescreen',
+            'marketing',
+            'mobile-app-banner',
+            'navigation',
+            'onboarding',
+            'onboarding-tasks',
+            'product-custom-fields',
+            'remote-inbox-notifications',
+            'remote-free-extensions',
+            'payment-gateway-suggestions',
+            'shipping-label-banner',
+            'subscriptions',
+            'store-alerts',
+            'transient-notices',
+            'woo-mobile-welcome',
+            'wc-pay-promotion',
+            'wc-pay-welcome-page',
+            'launch-your-store',
+        ];
+        return array_diff($features, $disabled_features);
     },
-    51,
-    0
+    0,
+    1
 );
