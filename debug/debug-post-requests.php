480fb895ba6fa6ac30270b91dc05d21989fc88c0
Viktor Szépe <viktor@szepe.net>
2025-01-17 21:44:23 +0100

    fix cs

--- debug/debug-post-requests.php
+++ debug/debug-post-requests.php
@@ -4,4 +4,6 @@
  * Insert this snippet at the top of wp-config.php
  */
 
-if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') { error_log('POSTed:'.$_SERVER['REQUEST_URI'].json_encode($POST)); }
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    error_log('POSTed:'.$_SERVER['REQUEST_URI'].json_encode($POST));
+}
