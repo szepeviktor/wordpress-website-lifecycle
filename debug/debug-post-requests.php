efdc375d29f71a769f67963d432587ea9df85a69
Viktor Szépe <viktor@szepe.net>
2025-03-01 17:36:50 +0100

    Add alternative target to debug-post-requests.php

--- debug/debug-post-requests.php
+++ debug/debug-post-requests.php
@@ -5,5 +5,6 @@
  */
 
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    // file_put_contents(WP_CONTENT_DIR.'/debug-request.log',
     error_log(sprintf('POSTed:%s:%s', $_SERVER['REQUEST_URI'], json_encode($_POST)));
 }
