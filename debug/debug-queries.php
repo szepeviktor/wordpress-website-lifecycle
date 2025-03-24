f5fb71e4750fb21a30c3799ddf893442eb378c52
Viktor Szépe <viktor@szepe.net>
2025-03-24 01:36:32 +0000

    Add debug-queries

---
+++ debug/debug-queries.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Insert this snippet at the top of the _do_query method
+ *
+ * wp-includes/class-wpdb.php
+ */
+
+file_put_contents(
+    WP_CONTENT_DIR.'/debug-queries.log',
+    sprintf(
+        '%.2f %s@%.4f %s %s %s%c',
+        microtime(true),
+        php_sapi_name() === 'cli' ? 'CLI' : $_SERVER['REMOTE_ADDR'],
+        $_SERVER['REQUEST_TIME_FLOAT'],
+        $_SERVER['REQUEST_METHOD'],
+        $_SERVER['REQUEST_URI'],
+        $query,
+        10
+    ),
+    FILE_APPEND | LOCK_EX
+);
