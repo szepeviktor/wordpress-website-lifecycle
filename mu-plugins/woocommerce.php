11595a65429ed5845d6ea48719a9f76ee07f876c
Viktor Szépe <viktor@szepe.net>
2025-04-19 02:33:48 +0000

    Add all WC feature descriptions

--- mu-plugins/woocommerce.php
+++ mu-plugins/woocommerce.php
@@ -24,17 +24,18 @@ add_filter(
             // Store API rate limiting
             // https://github.com/woocommerce/woocommerce/blob/trunk/plugins/woocommerce/src/StoreApi/docs/rate-limiting.md
             'rate_limit_checkout',
-            // Remote logging at public-api.wordpress.com
+            // Remote logging to public-api.wordpress.com
             // https://developer.woocommerce.com/2024/09/23/recent-updates-to-error-handling-and-optional-remote-error-logging/
             'remote_logging',
             // Coming soon mode and admin bar badge
             // https://woocommerce.com/document/configuring-woocommerce-settings/coming-soon-mode/
             'site_visibility_badge',
+
             // From includes/react-admin/feature-config.php
             // A toolbar-like area
             // https://woocommerce.com/document/home-screen/#section-6
             'activity-panels',
-            // Analytics and Sales Reports
+            // Analytics and sales reports
             // https://woocommerce.com/document/woocommerce-analytics/
             'analytics',
             // Gutenberg blocks
@@ -49,44 +50,83 @@ add_filter(
             // Coupons
             // https://woocommerce.com/document/coupon-management/
             'coupons',
-            // Onboarding setup for merchants
+            // Onboarding setup
             'core-profiler',
             // Design the look and feel of your online store without code
             // https://woocommerce.com/document/woocommerce-customize-your-store/
             'customize-store',
+            // Measure user satisfaction
             'customer-effort-score-tracks',
+            // Background product import
             'import-products-task',
+            // Import fashion products from experimental_fashion_sample_9_products.csv
             'experimental-fashion-sample-products',
+            // Automated shipping settings
             'shipping-smart-defaults',
+            // Interactive shipping settings
             'shipping-setting-tour',
-            'homescreen', // Marketplace depends on this
+            // Centralized dashboard - Marketplace depends on this
+            'homescreen',
+            // Marketing menu
             'marketing',
+            // Core does not have .min.asset.php files
             'minified-js',
+            // Promote mobile application
             'mobile-app-banner',
+            // Deprecated, see core-profiler
             'onboarding',
+            // Setup Checklist
             'onboarding-tasks',
+            // Pattern Toolkit (PTK)
+            // https://woocommerce.com/document/woocommerce-store-editing/patterns-template-parts/
             'pattern-toolkit-full-composability',
+            // Ready to publish?
             'product-pre-publish-modal',
+            // Custom Fields on Products
+            // https://woocommerce.com/document/custom-product-fields/
             'product-custom-fields',
+            // Notifications from WooCommerce.com
             'remote-inbox-notifications',
+            // Promote free extensions
             'remote-free-extensions',
+            // Promote payment gateways
+            // https://github.com/woocommerce/woocommerce/blob/trunk/plugins/woocommerce/client/admin/docs/features/payment-gateway-suggestions.md
             'payment-gateway-suggestions',
-            'printful', // Show printful.com banner
-            'settings', // React Settings page
+            // Promote printful.com
+            'printful',
+            // React Settings page
+            'settings',
+            // Promote shipping labels
             'shipping-label-banner',
+            // Subscriptions core support
             'subscriptions',
+            // activity-panels? remote-inbox-notifications?
             'store-alerts',
+            // Snackbar notices
+            // https://github.com/woocommerce/woocommerce/blob/trunk/plugins/woocommerce/client/admin/docs/features/transient-notices.md
             'transient-notices',
+            // Welcome experience on mobile
             'woo-mobile-welcome',
+            // Promote WooCommerce Payments
             'wc-pay-promotion',
+            // Introduction to WooCommerce Payments
             'wc-pay-welcome-page',
+            // Async loading of category selection field
             'async-product-editor-category-field',
+            // Launch Your Store task
+            // https://developer.woocommerce.com/roadmap/launch-your-store-task/
             'launch-your-store',
+            // Product form post type
             'product-editor-template-system',
+            // WordPress Blueprint compatible JSON formats
             'blueprint',
+            // Modernizing PHP-based payment gateway settings pages
             'reactify-classic-payments-settings',
+            // Use horizon as calypso_env
             'use-wp-horizon',
+            // Plus (+) and minus (-) buttons for the number of items
             'add-to-cart-with-options-stepper-layout',
+            // Gutenberg Add to Cart block
             'blockified-add-to-cart',
         ];
         return array_diff($features, $disabled_features);
