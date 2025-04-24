4cf6ff612cfb1836075755d109fd1f894510bef6
Viktor Szépe <viktor@szepe.net>
2025-04-25 00:10:45 +0200

    Create debug-qm-filter.php

---
+++ debug/debug-qm-filter.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Insert this snippet in WP_Hook::apply_filters()
+ *
+ * wp-includes/class-wp-hook.php:328
+ */
+
+<?php
+
+global $wp_filter;
+$_do_debug_filter = ($wp_filter['wp_head'] ?? null) === $this;
+if ($_do_debug_filter) do_action('qm/start', 'filter');
+
+    // foreach ( $this->callbacks[ $priority ] as $the_ ) {
+    // call_user_func calls ...
+
+if ($_do_debug_filter) do_action('qm/lap', 'filter', QM_Util::populate_callback(['function' => $the_['function']])['name'] ?? 'N/A');
+
+    // }
+
+if ($_do_debug_filter) do_action('qm/stop', 'filter');
