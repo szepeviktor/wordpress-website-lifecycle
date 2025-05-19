64b3bc8fdaf1300567cb60a7f427f5b1ed6af2e3
Viktor Szépe <viktor@szepe.net>
2025-05-19 00:21:10 +0000

    Add http and email blocking to core-migration

--- mu-plugins/_core-migration.php
+++ mu-plugins/_core-migration.php
@@ -20,3 +20,25 @@ add_filter(
     0,
     1
 );
+
+// Block outbound HTTP requests
+add_filter(
+    'pre_http_request',
+    static function ($response, $parsed_args, $url) {
+        error_log('Outbound HTTP requests are blocked: '.$url);
+        return new WP_Error('outbound_http_blocked');
+    },
+    PHP_INT_MAX,
+    3
+);
+
+// Block email sending
+add_filter(
+    'pre_wp_mail',
+    static function ($return, $atts) {
+        error_log('Email sending is blocked: '.var_export($atts, true));
+        return false;
+    },
+    PHP_INT_MAX,
+    2
+);
