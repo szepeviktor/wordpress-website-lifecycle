513067f8802d88e85ec7727bf210ffaaeffb55b8
Viktor Szépe <viktor@szepe.net>
2025-08-29 05:06:47 +0000

    Disable Elementor tracking

--- mu-plugins/elementor.php
+++ mu-plugins/elementor.php
@@ -8,11 +8,22 @@
 add_action(
     'init',
     static function () {
+        if (!did_action('elementor/loaded')) {
+            return;
+        }
         require_once __DIR__.'/elementor/Admin_Notices.php';
         require_once __DIR__.'/elementor/Feedback.php';
         require_once __DIR__.'/elementor/Promotions.php';
     },
     // Before Elementor\Plugin::__construct
     -1,
+    0
+);
+
+// Disable tracking.
+add_filter(
+    'pre_option_elementor_allow_tracking',
+    '__return_empty_string',
+    10,
     0
 );
