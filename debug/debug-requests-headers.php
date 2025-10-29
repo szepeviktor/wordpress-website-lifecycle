d1a3dc68f429a4a2e51feb6d498a45ec4e329491
Viktor Szépe <viktor@szepe.net>
2025-10-29 17:57:09 +0100

    Add debug-requests-headers (#53)
    * Add debug-requests-headers
    
    * Refactor HTTP header logging into a function
    
    * Append newline to debug requests headers log
    
    Add a newline at the end of the log file for better readability.

---
+++ debug/debug-requests-headers.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Plugin Name: Log HTTP request headers (DBG)
+ * Plugin URI: https://github.com/szepeviktor/wordpress-website-lifecycle
+ */
+
+function _core_debug_requests_headers()
+{
+    if (!isset($_SERVER['REMOTE_ADDR'], $_SERVER['REQUEST_URI'])) {
+        return;
+    }
+    $log_items = [
+        sprintf('[%s] %s --- HTTP headers', date('c'), $_SERVER['REMOTE_ADDR']),
+        sprintf('%s %s %s', $_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'], $_SERVER['SERVER_PROTOCOL']),
+    ];
+    foreach (getallheaders() as $name => $value) {
+        $log_items[] = sprintf('%s: %s', $name, $value);
+    }
+    file_put_contents(
+        WP_CONTENT_DIR.'/debug-requests-headers.log',
+        implode("\n", $log_items)."\n",
+        FILE_APPEND | LOCK_EX
+    );
+}
+_core_debug_requests_headers();
