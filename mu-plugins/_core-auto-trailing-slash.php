4e7f9d8b4e307f5eafe0e2faa1b1b314e0220605
Viktor Szépe <viktor@szepe.net>
2026-01-24 15:52:28 +0100

    Implement trailing slash for no-dot paths
    Adds a filter to mod_rewrite_rules to enforce trailing slashes for URLs without dots.

---
+++ mu-plugins/_core-auto-trailing-slash.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Plugin Name: Trailing slash for no-dot paths
+ * Plugin URI: https://github.com/szepeviktor/wordpress-website-lifecycle
+ */
+
+add_filter(
+    'mod_rewrite_rules',
+    static function (string $rules) {
+        $insertion = <<<HTACCESS
+# Add trailing slash if doesn't end with / and doesn't contain a dot
+RewriteCond %{REQUEST_URI} !(\.|/$)
+RewriteRule ^(.+)$ /$1/ [R=301,L]
+
+HTACCESS;
+        $needle = "RewriteEngine On\n";
+        if (strpos($rules, $needle) === false) {
+            return $needle.$insertion.$rules;
+        }
+        return str_replace($needle, $needle.$insertion, $rules);
+    },
+    20,
+    1
+);
