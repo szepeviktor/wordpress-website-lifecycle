51f31d84c71392afc0e85fcc02c7dcb631d7ae92
Viktor Szépe <viktor@szepe.net>
2026-07-15 16:16:53 +0200

    Add FluentSMTP settings plugin

---
+++ mu-plugins/fluent-smtp.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Plugin Name: FluentSMTP settings
+ * Plugin URI: https://github.com/szepeviktor/wordpress-website-lifecycle
+ */
+
+add_action('fluent_mail_loading_app', static function () {
+    wp_add_inline_script(
+        'fluent_mail_admin_app_boot',
+        <<<'JS'
+window.FluentMail.addFilter(
+    'fluentmail_top_menus',
+    'remove_about_and_documentation',
+    function (menus) {
+        return menus.filter(function (menu) {
+            return !['support', 'docs'].includes(menu.route);
+        });
+    }
+);
+JS
+    );
+});
