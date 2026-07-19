d94f269864c050726caf5c853352aaa5cb8992c9
Viktor Szépe <viktor@szepe.net>
2026-07-19 11:57:56 +0200

    Create viktor-widget.php (#51)
    * Create viktor-widget.php
    
    * Fix line length

---
+++ mu-plugins/viktor-widget.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * Plugin Name: Viktor widget
+ * Plugin URI: https://github.com/szepeviktor/wordpress-website-lifecycle
+ */
+
+add_action('wp_dashboard_setup', 'szv_add_dashboard_widget', 10, 0);
+
+function szv_add_dashboard_widget()
+{
+    wp_add_dashboard_widget(
+        'szv_widget',
+        'Welcome developer!', // widget_name
+        'szv_render_dashboard_widget',
+        null,
+        null,
+        'normal', // context
+        'high' // priority
+    );
+}
+
+function szv_render_dashboard_widget($post, $callback_args)
+{
+    $profile_url = 'https://github.com/szepeviktor';
+    echo <<<HTML
+<div class="main">
+    <ul>
+        <li class="onboarding-server">
+            <a
+                href="{$profile_url}/debian-server-tools/blob/master/Onboarding.md#onboarding-for-developers"
+                target="_blank"
+            >Server info for developers</a>
+        </li>
+        <li class="onboarding-wordpress">
+            <a
+                href="{$profile_url}/wordpress-website-lifecycle/blob/master/README.md#onboarding-for-developers"
+                target="_blank"
+            >WordPress info for developers</a>
+        </li>
+        <li class="questions">
+            <a href="mailto:viktor@szepe.net">Questions?</a>
+        </li>
+    </ul>
+</div>
+HTML;
+}
