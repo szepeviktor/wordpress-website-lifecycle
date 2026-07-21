82c4a9baa0a3dd4f991626bc46928ab8b98f8863
Viktor Szépe <viktor@szepe.net>
2026-07-21 08:02:05 +0200

    Refactor query vars disabling logic in plugin
    Updated the action hook to improve code structure and clarity.

---
+++ mu-plugins/_core-disable-query-vars-parameters.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Plugin Name: Disable query vars parameters
+ * Plugin URI: https://github.com/szepeviktor/wordpress-website-lifecycle
+ */
+
+// permalink_structure must of enabled
+add_action(
+    'parse_request',
+    static function ($wp): {
+        if (is_admin()) {
+            return;
+        }
+        $requested_query_vars = array_intersect(
+            array_keys($_GET),
+            $wp->public_query_vars
+        );
+        if ($requested_query_vars !== []) {
+            $wp->query_vars = ['error' => '404'];
+        }
+    },
+    0,
+    1
+);
