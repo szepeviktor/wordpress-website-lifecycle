c70135dc6b1750d0d1474145da75d096b4a25f57
Viktor Szépe <viktor@szepe.net>
2026-07-21 19:06:10 +0200

    Enhance query var handling for REST requests
    Add a check for pretty REST requests to bypass query var blocking in admin and REST contexts.

--- mu-plugins/_core-disable-query-vars-parameters.php
+++ mu-plugins/_core-disable-query-vars-parameters.php
@@ -9,14 +9,24 @@
 add_action(
     'parse_request',
     static function ($wp) {
-        if (is_admin()) {
+        $is_pretty_rest_request = isset($wp->query_vars['rest_route'])
+            && !array_key_exists('rest_route', $_GET);
+        if (is_admin() || $is_pretty_rest_request) {
             return;
         }
+        $whitelist = [
+            's', // Search
+            'preview', // Post preview
+        ];
         $requested_query_vars = array_intersect(
             array_keys($_GET),
             $wp->public_query_vars
         );
-        if ($requested_query_vars !== []) {
+        $blocked_query_vars = array_diff(
+            $requested_query_vars,
+            $whitelist
+        );
+        if ($blocked_query_vars !== []) {
             $wp->query_vars = ['error' => '404'];
         }
     },
